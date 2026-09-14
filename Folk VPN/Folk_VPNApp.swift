9381415edced065f7177618a52f3f2726ca7e901
anantkrsingh <anantkrplay@gmail.com>
2026-09-14 10:16:17 +0530

    Initial Commit

---
+++ Folk VPN/Folk_VPNApp.swift
@@ -0,0 +1,17 @@
+//
+//  Folk_VPNApp.swift
+//  Folk VPN
+//
+//  Created by Anant Kumar on 14/09/26.
+//
+
+import SwiftUI
+
+@main
+struct Folk_VPNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Folk VPNTests/Folk_VPNTests.swift
@@ -0,0 +1,19 @@
+//
+//  Folk_VPNTests.swift
+//  Folk VPNTests
+//
+//  Created by Anant Kumar on 14/09/26.
+//
+
+import Testing
+@testable import Folk_VPN
+
+struct Folk_VPNTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
